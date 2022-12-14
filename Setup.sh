404e7d0f552b5dcec0957dddeb3ba80ac1fbbf5e
FM <120551519+FM-FIRES@users.noreply.github.com>
2022-12-14 15:12:26 +0600

    Create Setup.sh

---
+++ Setup.sh
@@ -0,0 +1,37 @@
+pip install lolcat
+echo '
+       __Hey guys {- _ -}
+   _  |@@|             
+  / \ \--/ __
+  ) O|----|  |   __
+ / / \ }{ /\ )_ / _\
+ )/  /\__/\ \__O (__ This is FM
+|/  (--/\--)    \__/
+/   _)(  )(_
+   ----------'| lolcat
+echo " "
+echo "\. ..Script By {F.M OFFICIAL}.../"| lolcat
+echo " "
+apt-get update -y
+apt-get upgrade -y
+apt install python -y
+apt install python2 -y
+apt install java -y
+apt install fish -y
+apt install wget -y
+apt install cmatrix -y
+apt install php -y
+apt install curl -y
+apt install zip -y
+apt install pip -y
+apt install openssl -y
+apt install figlet -y
+apt install toilet -y
+pip install --upgrade pip
+pip install bs4
+pip install requests
+pip install mechanize
+pip install requirements
+apt install git -y
+apt install ruby -y
+termux-setup-storage
